e80cd726c0906bc598bb751a7fbabbd35e20c21b
jeonjunhee93 <lllllljz@naver.com>
2025-06-11 21:28:25 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  base: './' // ✅ 이것 추가!
 })
